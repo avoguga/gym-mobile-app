eb502ba47b2bf11add1689ee4d3f09030980dba4
Gustavo <avoguga@gmail.com>
2022-12-13 02:18:49 -0300

    Created a new Expo app

---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5kVw9KCNNo4_HI9G8NcsQglUBW2QN7uw",
+  authDomain: "gym-app-ce7a7.firebaseapp.com",
+  projectId: "gym-app-ce7a7",
+  storageBucket: "gym-app-ce7a7.appspot.com",
+  messagingSenderId: "313118599086",
+  appId: "1:313118599086:web:8e74b026bcba36d6458a3f",
+};
+
+export const app = initializeApp(firebaseConfig);
