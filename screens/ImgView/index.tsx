396fe1abf36a1dc1faca1a904d3a99a8286aae91
Gustavo <avoguga@gmail.com>
2023-04-06 14:06:08 -0300

    fix things

--- screens/ImgView/index.tsx
+++ screens/ImgView/index.tsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Image, Platform } from "react-native";
 import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
 import { Button } from "@rneui/themed";
 
 export default function ImgView({ route, navigation }: any) {
+
+  const [rotation, setRotation] = useState("0deg");
+
+  useEffect(() => {
+    const updateRotation = () => {
+      if (Platform.OS === "web") {
+        setRotation(
+          window.screen.orientation.type === "portrait-primary"
+            ? "90deg"
+            : "0deg"
+        );
+      } else {
+        setRotation("0deg");
+      }
+    };
+
+    updateRotation();
+
+    if (Platform.OS === "web") {
+      window.addEventListener("resize", updateRotation);
+      return () => window.removeEventListener("resize", updateRotation);
+    }
+  }, []);
+
   // FUNCTION TO RETURN TO PREVIOUS SCREEN
   const handleBack = () => {
     navigation.goBack();
   };
 
-
   return (
     <>
-        <Button
-          title="Voltar para a tela inicial"
-          onPress={handleBack}
-          buttonStyle={{
-            position: "relative",
-            top: 0,
-            left: 0,
-            backgroundColor: "#c90087",
-          }}
-        />
+      <Button
+        title="Voltar para a tela inicial"
+        onPress={handleBack}
+        buttonStyle={{
+          position: "relative",
+          top: 0,
+          left: 0,
+          backgroundColor: "#c90087",
+        }}
+      />
       <ReactNativeZoomableView
         maxZoom={4}
         minZoom={0.5}
@@ -31,7 +54,7 @@ export default function ImgView({ route, navigation }: any) {
       >
         <Image
           source={{ uri: route.params.cardElements.img }}
-          style={styles.img}
+          style={[styles.img, { transform: [{ rotate: rotation }] }]}
         />
       </ReactNativeZoomableView>
     </>
@@ -49,7 +72,5 @@ const styles = StyleSheet.create({
     width: Platform.OS === "web" ? "200%" : "100%",
     height: Platform.OS === "web" ? "200%" : "100%",
     resizeMode: "contain",
-    // if it is an google browser, it will rotate the img 90 degrees
-    transform: [{ rotate: Platform.OS === "web" ? "90deg" : "0deg" }],
   },
 });
